86e34ecced68b2056506cb27a9cf7b3d581fb808
V THRISHATHI <tv1749@srmist.edu.in>
2026-04-09 21:23:38 +0530

    UC4: Display Hello with multiple names or default World

---
+++ UC5.java
@@ -0,0 +1,18 @@
+public class UC5 {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            System.out.print("Hello ");
+            
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i]);
+                
+                if (i < args.length - 1) {
+                    System.out.print(", ");
+                }
+            }
+        }
+    }
+}
