55f1ca224f642bf3f34158648ab79b3a02bc994a
V THRISHATHI <tv1749@srmist.edu.in>
2026-04-09 21:31:37 +0530

    UC5: Use enhanced for loop to display multiple names with default World

---
+++ UC5.2.java
@@ -0,0 +1,20 @@
+public class UC5.2 {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            System.out.print("Hello, ");
+
+            int count = 0;
+            for (String name : args) {
+                System.out.print(name);
+                count++;
+
+                if (count < args.length) {
+                    System.out.print(", ");
+                }
+            }
+        }
+    }
+}
